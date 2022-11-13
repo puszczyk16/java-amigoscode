2de822219a43884a57596e7f2562476b9098f7df
ciapatti <ciapatti79@gmail.com>
2022-11-13 10:27:33 +0100

    Added notes

---
+++ demo/notes.sh
@@ -0,0 +1,18 @@
+# PSQL
+\l
+\du
+CREATE DATABASE student
+
+CREATE ROLE amigoscode WITH LOGIN ENCRYPTED PASSWORD 'password';
+
+GRANT ALL PRIVILEGES ON DATABASE "student" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "student" TO amigoscode;
+
+DROP ROLE amigoscode;
+
+# Hibernate is one othe most known implementation of the JPA (Java Persistence API)
+# Framework responds for easier communication with databases
+# + fastest development
+# - delays in access time
+#
+#
