8e9243aa33a82d3c52fb91422894fbd3a5529d46
ciapatti <ciapatti79@gmail.com>
2022-12-06 21:14:37 +0100

    Added first API

--- demo/src/main/java/com/amigoscode/Main.java
+++ demo/src/main/java/com/amigoscode/Main.java
@@ -3,11 +3,19 @@ package com.amigoscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         // args is anything that comes from commandline
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello";
     }
 }
