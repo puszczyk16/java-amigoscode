77b772c3e0fe8ec6013cc95a4d0594839ca54981
Paweł Żmiejko <pzmiejko@gmail.com>
2022-12-06 23:58:11 +0100

    Added compare for record and class

--- demo/src/main/java/com/amigoscode/Main.java
+++ demo/src/main/java/com/amigoscode/Main.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @SpringBootApplication
 //@ComponentScan(basePackages = "com.amigoscode")
 //@EnableAutoConfiguration
@@ -18,7 +20,39 @@ public class Main {
 
     @GetMapping("/greet1")
     public GreetResponse blala() {
-        return new GreetResponse("Hello");
+        return new GreetResponse("Hello1");
     }
-    record GreetResponse(String greet123) {}
+    //records are classes that allow to achieve immutability
+    //equivalent class to record below is
+    /*
+    class GreetResponse {
+        private final String greet;
+        public GreetResponse(String bla) {
+            this.greet = bla;
+        }
+        public String getGreet() {
+            return greet;
+        }
+        @Override
+        public String toString() {
+            return "GreetResponse{" +
+                    "greet='" + greet + '\'' +
+                    '}';
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GreetResponse that = (GreetResponse) o;
+            return Objects.equals(greet, that.greet);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(greet);
+        }
+
+    }
+    */
+    record GreetResponse(String greet) {}
 }
